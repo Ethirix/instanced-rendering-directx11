4e4d6d1aaf3845c85572bca63691b517fd1619a8
Jack Wright <68773585+Ethirix@users.noreply.github.com>
2025-02-17 01:49:43 +0000

    Added Camera Moving Forwards

--- instanced-rendering-directx11/Engine/Shaders/PS.hlsl
+++ instanced-rendering-directx11/Engine/Shaders/PS.hlsl
@@ -4,16 +4,16 @@
 
 float4 PS_Main(VSOut input) : SV_TARGET
 {
-	float3 ambientOut = float3(0.2f, 0.2f, 0.2f);
+	float3 ambientOut = float3(0.1f, 0.1f, 0.1f);
 
-    float3 lightDir = float3(1, 1, 1);
+    float3 lightDir = normalize(-float3(-1, -1, -1));
     float diffuseIntensity = saturate(dot(input.Normal, lightDir));
-    float3 diffuseOut = diffuseIntensity * float3(0.8f, 0.8f, 0.8f);
+    float3 diffuseOut = diffuseIntensity * float3(1.0f, 1.0f, 1.0f);
 
     float3 eyeDirection = normalize(CameraPosition - input.Position.xyz);
     float3 halfVector = normalize(lightDir + eyeDirection);
-    float specularIntensity = pow(saturate(dot(input.Normal, halfVector)), 32);
-    float3 specularOut = specularIntensity * float3(1, 1, 1);
+    float specularIntensity = pow(saturate(dot(input.Normal, halfVector)), 128);
+    float3 specularOut = specularIntensity * float3(1, 1, 1) * 2;
 
-    return float4(input.Color + ambientOut + diffuseOut + specularOut, 1);
+    return float4(ambientOut * input.Color + diffuseOut * input.Color + specularOut, 1);
 }
